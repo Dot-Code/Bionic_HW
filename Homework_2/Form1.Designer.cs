ca432bd39a3678749d70002c08e8d64cf3f50629
Dot-Code <pp89@ukr.net>
2015-11-20 19:47:27 +0200

    First

---
+++ Homework_2/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Homework_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.New = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 67);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 67);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 67);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(176, 86);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 67);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(95, 86);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 67);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(14, 86);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 67);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(176, 159);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 67);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(95, 159);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 67);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(14, 159);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 67);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(150, 9);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(104, 33);
+            this.Start.TabIndex = 9;
+            this.Start.Text = "Start game";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(271, 38);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Player 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Player 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Your step";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Your step";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Location = new System.Drawing.Point(75, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(263, 237);
+            this.panel1.TabIndex = 16;
+            // 
+            // New
+            // 
+            this.New.Location = new System.Drawing.Point(150, 56);
+            this.New.Name = "New";
+            this.New.Size = new System.Drawing.Size(104, 33);
+            this.New.TabIndex = 17;
+            this.New.Text = "New game";
+            this.New.UseVisualStyleBackColor = true;
+            this.New.Click += new System.EventHandler(this.New_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 356);
+            this.Controls.Add(this.New);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cross Zero";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button New;
+    }
+}
+
---
+++ Homework_2/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Homework_2
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        private bool running;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += btn_Click;
+            button2.Click += btn_Click;
+            button3.Click += btn_Click;
+            button4.Click += btn_Click;
+            button5.Click += btn_Click;
+            button6.Click += btn_Click;
+            button7.Click += btn_Click;
+            button8.Click += btn_Click;
+            button9.Click += btn_Click;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            SignDraw(this.ActiveControl as System.Windows.Forms.Button);
+            this.ActiveControl.Enabled = false;
+        }
+        public bool PlayerX { get; set; }
+        public bool PlayerO { get; set; }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NewGame();
+            New.Enabled = false;
+        }
+
+        private void NewGame()
+        {
+            comboBox1.Items.Add("Zero");
+            comboBox1.Items.Add("Cross");
+
+            comboBox2.Items.Add("Zero");
+            comboBox2.Items.Add("Cross");
+            panel1.Enabled = false;
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("You must choose a 'Zero' or 'Cross'");
+                return;
+            }
+
+            if (comboBox1.SelectedItem == comboBox2.SelectedItem)
+            {
+                MessageBox.Show("A players must be diferent ");
+                return;
+            }
+
+            if (comboBox1.SelectedItem == "Cross")
+            {
+                PlayerX = true;
+                PlayerO = false;
+            }
+            else
+            {
+                PlayerX = false;
+                PlayerO = true;
+            }
+            running = true;
+            New.Enabled = true;
+            panel1.Enabled = true;
+            Start.Enabled = false;
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+            thread = new Thread(this.MethodCicle);
+            thread.IsBackground = true;
+            thread.Start();
+
+        }
+
+        private void MethodCicle()
+        {
+            System.Windows.Forms.Button[] myArray = { button1, button2, button3, button4, button5, button6, 
+                                                        button7, button8, button9 };
+            Random random = new Random();
+            while (running)
+            {
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    if (!running)
+                        return;
+                    myArray[i].BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+                }
+            }
+        }
+
+        private void SignDraw(System.Windows.Forms.Button element)
+        {
+            if (PlayerX)
+            {
+                DrawX(element);
+                PlayerX = false;
+                PlayerO = true;
+            }
+            else
+            {
+                DrawY(element);
+                PlayerX = true;
+                PlayerO = false;
+            }
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Button[] myArray = { button1, button2, button3, button4, button5, button6, 
+                                                        button7, button8, button9 };
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i].Text = "";
+                myArray[i].Enabled = true;
+            }
+            Start.Enabled = true;
+            New.Enabled = false;
+            panel1.Enabled = false;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            running = false;
+        }
+
+        public void DrawX(System.Windows.Forms.Button btnX)
+        {
+            btnX.Text = "X";
+            btnX.Font = new System.Drawing.Font(btnX.Font, System.Drawing.FontStyle.Bold);
+        }
+
+        public void DrawY(System.Windows.Forms.Button btnO)
+        {
+            btnO.Text = "O";
+            btnO.Font = new System.Drawing.Font(btnO.Font, System.Drawing.FontStyle.Bold);
+        }
+    }
+}
